4779f72e422b980ec0bbd87caf06dbed28ae6449
JoanRibot <jribot@cifpfbmoll.eu>
2020-12-09 12:38:07 +0100

    metiendo esquema

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-import services.src.domain.subir_json 
+from services.src.domain.subir_json  import subir_jason_bd
 
+
+subir_jason_bd()
--- services/src/domain/subir_json.py
+++ services/src/domain/subir_json.py
@@ -1,22 +1,22 @@
 import pymongo
 from ..programa import json
+def subir_jason_bd():
+    try:
+        uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.wiedk.mongodb.net/test"
+        client = pymongo.MongoClient(uri)
+        client.server_info()
+        print ("Conectado al servidor")
+    except pymongo.errors.ServerSelectionTimeoutError as error:
+        print( "Error al conectar al servidor") % error
+    except pymongo.errors.CollectionInvalid as error:
+        print("could not connect to MongoDB") % error
 
-try:
-    uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.wiedk.mongodb.net/test"
-    client = pymongo.MongoClient(uri)
-    client.server_info()
-    print ("Conectado al servidor")
-except pymongo.errors.ServerSelectionTimeoutError as error:
-    print( "Error al conectar al servidor") % error
-except pymongo.errors.CollectionInvalid as error:
-    print("could not connect to MongoDB") % error
+    db = client.proyecto
+    collection = db.diccionario
+    collection.drop()
 
-db = client.proyecto
-collection = db.diccionario
-collection.drop()
-
-try:    
-    collection.insert_one(json)
-    print("Successfully added")
-except Exception as error:
-    print("Error saving data")
+    try:    
+        collection.insert_one(json)
+        print("Successfully added")
+    except Exception as error:
+        print("Error saving data")
