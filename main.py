e2b6d83b71c2afb5251fb5b89ab09911008aa2c7
JoanRibot <jribot@cifpfbmoll.eu>
2020-12-10 11:43:45 +0100

    Hotfix, separando diccionarios, cambiando nombres, mejorando documentacion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from services.src.extraer_atributos_html  import sube_colecciones, Atributos
-from services.src.html_string import htmls
+from services.src.extraer_atributos_html  import sube_colecciones
 from services.src.conexion_bd import borrar_coleccion
 
 sube_colecciones(htmls,Atributos) #subir los diccionarios a la coleccion de la Base de Datos
