2b0d392a2a884f6f87d14b932356c3341c3275f1
JoanRibot <jribot@cifpfbmoll.eu>
2021-11-17 18:38:24 +0100

    [fix, quitando valores estraños html]

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from services.src.extraer_atributos_html  import menus_completos, Atributos
 from services.src.html_string import htmls
 from services.src.conexion_bd import borrar_coleccion
 
-# menus_completos(htmls,Atributos) #subir los diccionarios a la coleccion de la Base de Datos
+menus_completos(htmls,Atributos) #subir los diccionarios a la coleccion de la Base de Datos
 
 
-borrar_coleccion() ##borrar la coleccion de la Base de Datos
+# borrar_coleccion() ##borrar la coleccion de la Base de Datos
